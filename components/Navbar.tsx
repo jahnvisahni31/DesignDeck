0be8eed71bcb1dd398f398217d1c33beff66a292
Jahnvi sahni <121384173+jahnvisahni31@users.noreply.github.com>
2024-10-02 19:54:40 +0530

    Revert "FEATURE: Add navbar and faq page"

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,46 +24,35 @@ function Navbar({
 
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
-      <div className="flex-grow flex justify-center">
-        <ul className="flex">
-          {/* Renders nav elements */}
-          {navElements.map((item: ActiveElement | any) => (
-            <li
-              key={item.name}
-              onClick={() => {
-                if (Array.isArray(item.value)) return; // Prevents selection of shapes menu
-                handleActiveElement(item);
-              }}
-              className={`group px-2.5 py-5 flex justify-center items-center
+      <Image src="/assets/logo.png" alt="Logo" width={66} height={26} />
+
+      <ul className="flex">
+        {/* Renders nav elements */}
+        {navElements.map((item: ActiveElement | any) => (
+          <li
+            key={item.name}
+            onClick={() => {
+              if (Array.isArray(item.value)) return; // Prevents selection of shapes menu
+              handleActiveElement(item);
+            }}
+            className={`group px-2.5 py-5 flex justify-center items-center
           ${
             isActive(item.value)
               ? "bg-primary-green"
               : "hover:bg-primary-grey-200"
           }
           `}
-            >
-              {Array.isArray(item.value) ? ( // Renders shapes menu
-                <ShapesMenu
-                  item={item}
-                  activeElement={activeElement}
-                  imageInputRef={imageInputRef}
-                  handleActiveElement={handleActiveElement}
-                  handleImageUpload={handleImageUpload}
-                />
-              ) : item.value === "comments" ? ( // Renders comments cursor
-                <NewThread>
-                  <Button className="relative w-5 h-5 object-contain">
-                    <Image
-                      src={item.icon}
-                      alt={item.name}
-                      fill
-                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 20vw"
-                      className={isActive(item.value) ? "invert" : ""}
-                    />
-                  </Button>
-                </NewThread>
-              ) : (
-                // Renders other nav elements
+          >
+            {Array.isArray(item.value) ? ( // Renders shapes menu
+              <ShapesMenu
+                item={item}
+                activeElement={activeElement}
+                imageInputRef={imageInputRef}
+                handleActiveElement={handleActiveElement}
+                handleImageUpload={handleImageUpload}
+              />
+            ) : item.value === "comments" ? ( // Renders comments cursor
+              <NewThread>
                 <Button className="relative w-5 h-5 object-contain">
                   <Image
                     src={item.icon}
@@ -73,11 +62,22 @@ function Navbar({
                     className={isActive(item.value) ? "invert" : ""}
                   />
                 </Button>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
+              </NewThread>
+            ) : (
+              // Renders other nav elements
+              <Button className="relative w-5 h-5 object-contain">
+                <Image
+                  src={item.icon}
+                  alt={item.name}
+                  fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 20vw"
+                  className={isActive(item.value) ? "invert" : ""}
+                />
+              </Button>
+            )}
+          </li>
+        ))}
+      </ul>
 
       <ActiveUsers />
     </nav>
