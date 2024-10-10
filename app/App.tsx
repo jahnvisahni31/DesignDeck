ee472f06d32cb1f28aae905971385b53c9ff4098
J Shiva Shankar <144578658+STXRSHIVA@users.noreply.github.com>
2024-10-10 17:46:38 +0530

    fixed overflow of cardfooter

--- app/App.tsx
+++ app/App.tsx
@@ -12,13 +12,13 @@ export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const icons = {
-    chevron: <ChevronDown fill="currentColor" size={16} height={undefined} width={undefined} className="some-class-name" />, 
+    chevron: <ChevronDown fill="currentColor" size={16} height={undefined} width={undefined} className="some-class-name" />,
     hammer: <ImHammer />,
     dev: <ImDeviantart />,
     slide: <ImNewspaper />,
     download: <ImDownload />,
   };
-  
+
   const menuItems = [
     "Profile",
     "Dashboard",
@@ -32,14 +32,14 @@ export default function App() {
   const { systemTheme, theme } = useTheme();
   const currentTheme = theme === "dark" ? systemTheme : theme;
   const [darkMode, setDarkMode] = useState(false);
-  
+
   useEffect(() => {
     setDarkMode(currentTheme === 'dark');
   }, [currentTheme]);
 
   return (
     <>
-      <NavbarComponent isLoggedIn={isLoggedIn} setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} /> 
+      <NavbarComponent isLoggedIn={isLoggedIn} setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />
       <div className={`min-h-screen ${darkMode ? "bg-black text-white" : "bg-gradient-to-r from-gray-300 via-white to-gray-200 text-black"} font-sans`}>
         <div className={`py-4 ${darkMode ? "bg-black" : "bg-gradient-to-r from-gray-300 via-white to-gray-200 text-black"}`}>
           <div className="flex justify-around items-center">
@@ -75,83 +75,83 @@ export default function App() {
         </div>
 
         <div className={` ${darkMode ? " text-white " : " text-black"} py-20 mx-10 `}>
-      <div className="flex justify-center text-2xl m-4 pb-14">
+          <div className="flex justify-center text-2xl m-4 pb-14">
             Features
           </div>
-        <div className="max-w-8xl max-h-full mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 ">
-        <div className={` ${darkMode ? " text-white " : " text-neutral-900"} bg-none  shadow-2xl rounded-lg  text-center`}>
+          <div className="max-w-8xl max-h-full mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 ">
+            <div className={` ${darkMode ? " text-white " : " text-neutral-900"} bg-none  shadow-2xl rounded-lg  text-center`}>
 
-            <Card
-              isFooterBlurred
-              radius="lg"
-              className="border-none"
-            >
-              <Image
-                alt="Woman listing to music"
-                className=" object-fill w-full h-full"
-                height={370}
-                src="five.webp"
-                width={500}
-              />
-              <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 transition-all duration-300 ease-in-out hover:bg-black/50 hover:scale-105 ">
-              <p className="font-medium text-justify text-black " >Live Collaboration</p>
-                <Button className="text-tiny text-white bg-black/60" variant="flat" color="default" radius="lg" size="sm">
-                  Notify me
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-          <div className={`  bg-none shadow-2xl rounded-lg  text-center`}>
-            
-            <Card
-              isFooterBlurred
-              radius="lg"
-              className="border-none"
-            >
-              <Image
-                alt="Woman listing to music"
-                className="object-cover w-full h-full"
-                height={370}
-                src="four.webp"
-                width={500}
-              />
-              <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 transition-all duration-300 ease-in-out hover:bg-black/50 hover:scale-105">
-              
-                <p className=" font-light text-white text-justify">Real-Time Updates</p>
-                <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-                  <a href="/workspace">
-                  Notify me
-                  </a>
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-          
-          <div className={` ${darkMode ? " text-white " : " text-neutral-900"} bg-none shadow-2xl rounded-lg  text-center`}>
-            
-            <Card
-              isFooterBlurred
-              radius="lg"
-              className="border-none"
-            >
-              <Image
-                alt="Woman listing to music"
-                className="  object-cover w-full h-full"
-                height={370}
-                src="two.webp"
-                width={500}
-              />
-              <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 transition-all duration-300 ease-in-out hover:bg-black/50 hover:scale-105">
-              
-              
+              <Card
+                isFooterBlurred
+                radius="lg"
+                className="border-none "
+              >
+                <Image
+                  alt="Woman listing to music"
+                  className=" object-fill w-full h-full"
+                  height={370}
+                  src="five.webp"
+                  width={500}
+                />
+                <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[93%] mb-2 right-2 shadow-small  mr-2 z-10 transition-all duration-300 ease-in-out hover:bg-black/50 hover:scale-105 ">
+                  <p className="font-medium text-justify text-black " >Live Collaboration</p>
+                  <Button className="text-tiny text-white bg-black/60" variant="flat" color="default" radius="lg" size="sm">
+                    Notify me
+                  </Button>
+                </CardFooter>
+              </Card>
+            </div>
+            <div className={`  bg-none shadow-2xl rounded-lg  text-center`}>
+
+              <Card
+                isFooterBlurred
+                radius="lg"
+                className="border-none"
+              >
+                <Image
+                  alt="Woman listing to music"
+                  className="object-cover w-full h-full"
+                  height={370}
+                  src="four.webp"
+                  width={500}
+                />
+                <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[93%] mb-2 right-2 shadow-small  mr-2 z-10 transition-all duration-300 ease-in-out hover:bg-black/50 hover:scale-105">
+
+                  <p className=" font-light text-white text-justify">Real-Time Updates</p>
+                  <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+                    <a href="/workspace">
+                      Notify me
+                    </a>
+                  </Button>
+                </CardFooter>
+              </Card>
+            </div>
+
+            <div className={` ${darkMode ? " text-white " : " text-neutral-900"} bg-none shadow-2xl rounded-lg  text-center`}>
+
+              <Card
+                isFooterBlurred
+                radius="lg"
+                className="border-none"
+              >
+                <Image
+                  alt="Woman listing to music"
+                  className="  object-cover w-full h-full"
+                  height={370}
+                  src="two.webp"
+                  width={500}
+                />
+                <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[93%] mb-2 right-2 shadow-small  mr-2 z-10 transition-all duration-300 ease-in-out hover:bg-black/50 hover:scale-105">
+
+
                   <p className="text-white text-md font-sans">Text Addition</p>
                   {/* <p className="text-black text-tiny">Add text to designs and adjust font size, weight, and style.</p> */}
-                
-                <Button  color="default" className="  text-tiny font-sans text-white bg-black/60" variant="flat"  radius="lg" size="sm">
-                  Notify Me
-                </Button>
-              </CardFooter>
-              {/* <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+
+                  <Button color="default" className="  text-tiny font-sans text-white bg-black/60" variant="flat" radius="lg" size="sm">
+                    Notify Me
+                  </Button>
+                </CardFooter>
+                {/* <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
                 <div>
                   <p className="text-black">Text Addition</p>
                 </div>
@@ -159,10 +159,10 @@ export default function App() {
                   Notify Me
                 </Button>
               </CardFooter> */}
-            </Card>
+              </Card>
+            </div>
           </div>
-          </div>
-          </div>
+        </div>
 
         <div className={` ${darkMode ? "bg-black text-white " : "bg-gradient-to-r from-gray-300 via-white to-gray-200 text-black"} py-20 px-10 my-40`}>
           <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-center">
@@ -178,7 +178,7 @@ export default function App() {
             <div className="flex items-center mt-10 lg:mt-0">
               <div className="text-right mr-4 flex-col">
                 <div className="text-2xl font-semibold justify-start">GitHub</div>
-                
+
                 <div className="flex flex-row">
                   <div className="h-6 w-6 rounded-full bg-gradient-to-r from-green-400 to-yellow-400 p-8 m-3"></div>
                   <div className="flex flex-col justify-center items-center">
